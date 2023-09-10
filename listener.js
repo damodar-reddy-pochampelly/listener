1bcdcdf96705d9c3e567fb3efe0e69ee661cdf82
damodarreddy <damodarreddy18107@gmail.com>
2023-09-10 20:40:27 +0000

    changed listener.js

--- listener.js
+++ listener.js
@@ -2,8 +2,6 @@ const io = require("socket.io")();
 const crypto = require("crypto");
 const mongoose = require("mongoose");
 
-mongoose.set("strictQuery", true);
-
 // Create a MongoDB connection
 mongoose.connect(
   "mongodb+srv://damodarreddy18107:bgmjge181078@cluster0.vg9l0jd.mongodb.net/?retryWrites=true&w=majority",
@@ -32,32 +30,39 @@ io.on("connection", (socket) => {
 
     // Process each encrypted message
     encryptedMessages.forEach((encryptedMessage) => {
-      // Decrypt the message using your decryption algorithm (e.g., aes-256-ctr)
-      const decryptionKey = crypto.randomBytes(32); // Use the appropriate key
-      const iv = crypto.randomBytes(16); // Use the appropriate IV
-      const decipher = crypto.createDecipheriv(
-        "aes-256-ctr",
-        decryptionKey,
-        iv
-      );
-      let decryptedMessage = decipher.update(encryptedMessage, "hex", "utf8");
-      decryptedMessage += decipher.final("utf8");
+      try {
+        // Decrypt the message using your decryption algorithm (e.g., aes-256-ctr)
+        const decryptionKey = crypto.randomBytes(32); // Use the appropriate key
+        const iv = crypto.randomBytes(16); // Use the appropriate IV
+        const decipher = crypto.createDecipheriv(
+          "aes-256-ctr",
+          decryptionKey,
+          iv
+        );
+        let decryptedMessage = decipher.update(encryptedMessage, "hex", "utf8");
+        decryptedMessage += decipher.final("utf8");
 
-      // Output the decrypted message (as a string)
-      console.log("Decrypted message:", decryptedMessage);
+        // Check if the decrypted message is valid JSON
+        const parsedMessage = JSON.parse(decryptedMessage);
 
-      // Create a new TimeSeries document and save it to MongoDB
-      const timeSeriesData = new TimeSeries({
-        data: JSON.parse(decryptedMessage),
-      });
-      timeSeriesData
-        .save()
-        .then(() => {
-          console.log("Saved to MongoDB");
-        })
-        .catch((error) => {
-          console.error("Error saving to MongoDB:", error);
+        // Output the decrypted message (as a string)
+        console.log("Decrypted message:", parsedMessage);
+
+        // Create a new TimeSeries document and save it to MongoDB
+        const timeSeriesData = new TimeSeries({
+          data: parsedMessage,
         });
+        timeSeriesData
+          .save()
+          .then(() => {
+            console.log("Saved to MongoDB");
+          })
+          .catch((error) => {
+            console.error("Error saving to MongoDB:", error);
+          });
+      } catch (error) {
+        console.error("Error parsing decrypted message:", error);
+      }
     });
   });
 });
